240a95031cbe159a1714d6060a50b339154547d2
Hrishikesh Kalita <hrishi282k@gmail.com>
2023-07-21 19:39:15 +0530

    created checkout page along with checkout items and checkout cart fucntionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication";
 import Shop from "./routes/shop/shop.component";
+import CheckOut from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         {/* index is attribute which says index="true" when we match parent / it should render home as initail */}
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<CheckOut />} />
       </Route>
     </Routes> //this Routes and Route are able to connect to the url and render the appropriate page becaus eit is nested inside BrowserRouter
   );
--- src/Components/cart-dropdown/cart-dropdown.component.jsx
+++ src/Components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./cart-dropdown.styles.scss";
 
@@ -8,7 +9,14 @@ import CartItem from "../cart-item/cart-item.componet";
 import { CartContext } from "../../contexts/cart.context";
 
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const handleCheckOutOnCLick = () => {
+    setIsCartOpen(false);
+    navigate("/checkout");
+  };
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -16,8 +24,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={handleCheckOutOnCLick}>GO TO CHECKOUT</Button>
     </div>
   );
 };
